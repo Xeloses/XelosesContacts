3be81c21f793c36de8c542627acf7df616b8199a
Baertram <1195208+Baertram@users.noreply.github.com>
2025-01-25 17:28:47 +0100

    Added LibCustomMenu & pChat displayName

--- core/contextmenu.lua
+++ core/contextmenu.lua
@@ -43,7 +43,21 @@ local function onSocilaListRowMouseUp(list, control, button, upInside, category)
     end
 end
 
+--Get the @displayName from the current chat message where one opened the player contextMenu (if pChat is loaded and
+--the pChat settings add the @displayName at the currently clicked chat message)
+local function getPChatMessageDisplayName(player_name)
+    --As the XelosesContacts.txt adds an optional dependency to pChat, pChat's player contextmenu should be executed before
+    --XelosesContacts chat player contextmenu appears. Means: pChat will return the currently found displayName in the variable
+    --pChat.lastCheckDisplayNameData = { displayName=guildMemberOrFriendDisplayname, index=guildOrFriendIndexFound, isOnline=isOnline, type = "guild" or "friend"}
+    -->This variable is currently only filled if the user activated pChat settings to add the "Teleport to" context menu entries
+    if pChat and pChat.lastCheckDisplayNameData then
+        return pChat.lastCheckDisplayNameData.displayName
+    end
+    return player_name
+end
+
 function XC:SetupChatContextMenu()
+    --[[
     local ShowPlayerContextMenu = CHAT_SYSTEM.ShowPlayerContextMenu
     CHAT_SYSTEM.ShowPlayerContextMenu = function(chat, player_name, raw_name)
         local menu = ShowPlayerContextMenu(chat, player_name, raw_name)
@@ -55,9 +69,26 @@ function XC:SetupChatContextMenu()
         ShowMenu(chat)
         return menu
     end
+   ]]
+
+   --Beartram 20250125 - Example how to use LibCustomMenu here
+   --AddCustomMenuItem function accepts the same params as AddMenuItem, and LibCustomMenu supports submenus too (benefit) if needed
+   local function chatPlayerContextMenuAddition(player_name, raw_Name)
+       player_name = getPChatMessageDisplayName(player_name)
+       local target_name = XC:validateAccountName(player_name)
+       if (target_name) then
+           local menu_item = XC:createContactMenuItem(target_name)
+           if (menu_item) then
+               AddCustomMenuItem(menu_item.name, menu_item.callback)
+               ShowMenu() --shows at current MouseOverControl (where we right clicked)
+           end
+       end
+   end
+   LibCustomMenu:RegisterPlayerContextMenu(chatPlayerContextMenuAddition, LibCustomMenu.CATEGORY_LAST)
 end
 
 function XC:SetupGuildRosterContextMenu()
+    --Beartram 20250125 - Do not overwrite! Use LibCustomMenu API please!
     local onMouseUp = GUILD_ROSTER_KEYBOARD.GuildRosterRow_OnMouseUp
     GUILD_ROSTER_KEYBOARD.GuildRosterRow_OnMouseUp = function(list, control, button, upInside)
         onMouseUp(list, control, button, upInside)
@@ -66,6 +97,7 @@ function XC:SetupGuildRosterContextMenu()
 end
 
 function XC:SetupGroupWindowContextMenu()
+    --Beartram 20250125 - Do not overwrite! Use LibCustomMenu API please!
     local onMouseUp = GROUP_LIST.GroupListRow_OnMouseUp
     GROUP_LIST.GroupListRow_OnMouseUp = function(list, control, button, upInside)
         onMouseUp(list, control, button, upInside)
@@ -74,6 +106,7 @@ function XC:SetupGroupWindowContextMenu()
 end
 
 function XC:SetupFriendListContextMenu()
+    --Beartram 20250125 - Do not overwrite! Use LibCustomMenu API please!
     local onMouseUp = FRIENDS_LIST.FriendsListRow_OnMouseUp
     FRIENDS_LIST.FriendsListRow_OnMouseUp = function(list, control, button, upInside)
         onMouseUp(list, control, button, upInside)
@@ -82,6 +115,7 @@ function XC:SetupFriendListContextMenu()
 end
 
 function XC:SetupIgnoreListContextMenu()
+    --Beartram 20250125 - Do not overwrite! Use LibCustomMenu API please!
     local onMouseUp = IGNORE_LIST.IgnoreListPanelRow_OnMouseUp
     IGNORE_LIST.IgnoreListPanelRow_OnMouseUp = function(list, control, button, upInside)
         onMouseUp(list, control, button, upInside)
