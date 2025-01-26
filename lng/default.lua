2bf3f0a64519e4e83ccbae100a871abdb89e4bc7
Xeloses <37206095+Xeloses@users.noreply.github.com>
2025-01-26 03:20:31 +0300

    Update default.lua
    replace ZO_CreateStringId() with SafeAddString()

--- lng/default.lua
+++ lng/default.lua
@@ -1,10 +1,10 @@
 local S = GetString
-local _L = ZO_CreateStringId --This will add the String constant with version = 1, so the other language files should add version 2 or higher to overwrite it via function SafeAddString
+local _L = function(id, value, version) return SafeAddString(id, value, version or 2) end
 
 local function L(id, value, ...)
     local params = { ... }
     if (params and #params > 0) then value = string.format(value, ...) end
-    return _L("XELCONTACTS_" .. id, value)
+    return _L(_G["XELCONTACTS_" .. tostring(id)], value)
 end
 
 -- ============================
